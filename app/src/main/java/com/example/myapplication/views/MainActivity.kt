3f20643209375e23cd1ca659315231d7348ec6b1
Noé <noeebacke@gmail.com>
2023-03-31 16:55:36 +0200

    Set différents images by the weathercode, the images was create by colleague very generous

--- app/src/main/java/com/example/myapplication/views/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/views/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.views
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -10,7 +9,6 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.database.CityRoomDatabase
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.entity.WeatherCityEntity
-import com.example.myapplication.model.Weather
 import com.example.myapplication.repository.WeatherCityRepository
 import com.example.myapplication.retrofit.RetrofitBuilder
 import com.example.myapplication.viewmodels.WeatherViewModel
@@ -28,14 +26,6 @@ class MainActivity : AppCompatActivity() {
         latitude = binding.inputLatitude.text.toString().toFloat()
         setResultOfSearch()
     }
-
-//    private fun supressDataOfTextLAtitude(){
-//        binding.inputLatitude.text?.clear()
-//    }
-//
-//    private fun supressDataOfTextLongitude(){
-//        binding.inputLongitude.text?.clear()
-//    }
 
     private fun setResultOfSearch(){
         val retrofitBuilder = RetrofitBuilder.apiService
@@ -70,15 +60,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("DiscouragedApi")
-    private fun setCurrentMeteo(time:String,meteo:String) {
-        val value = meteo
-        val filename = "w_$value"
-        val resID = resources.getIdentifier(filename, "drawable", packageName)
-        binding.img.setImageResource(resID)
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -87,19 +68,9 @@ class MainActivity : AppCompatActivity() {
         var button = binding.eventClick
         button.setOnClickListener { searchingButton() }
 
-//        var inputLat = binding.inputLatitude
-//        var inputLong = binding.inputLongitude
-//
-//        inputLong.setOnFocusChangeListener { view, b -> supressDataOfTextLongitude() }
-//        inputLat.setOnFocusChangeListener { view, b -> supressDataOfTextLAtitude() }
-
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
 
         weatherviewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
-        val text = binding.returnData.text
-        val text2 = binding.returnData2.text
-        val text3 = binding.returnData3.text
-
 
 
         // Tout le code en dessous, ne doit pas exister dans le MainActivity, il doit être présent dans le ViewModel
